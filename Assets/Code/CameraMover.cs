75bf6a292de1fe7e520d0dcc7ed948d4fbfb0c81
Aleksey-Golub <alekseigolub.ag@gmail.com>
2024-07-21 15:44:38 +0200

    add Camera movement

---
+++ Assets/Code/CameraMover.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+internal class CameraMover : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Vector3 _offset;
+
+    private void LateUpdate()
+    {
+        transform.position = _player.position + _offset;
+    }
+}

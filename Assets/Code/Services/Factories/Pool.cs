d694b25df0bb1f79e35c2b711254ca057a0d73d2
Aleksey-Golub <alekseigolub.ag@gmail.com>
2024-08-10 20:42:09 +0200

    add Pool as composition for factories

---
+++ Assets/Code/Services/Factories/Pool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal class Pool<T> where T : MonoBehaviour
+{
+    private readonly Queue<T> _pool;
+    private readonly T _prefab;
+    private readonly Transform _parent;
+
+    public Pool(T prefab, Transform parent, int initialPoolSize)
+    {
+        _prefab = prefab;
+        _parent = parent;
+
+        _pool = new Queue<T>(initialPoolSize);
+        FillPool(initialPoolSize);
+    }
+    internal T Get(Vector3 position, Quaternion rotation)
+    {
+        if (_pool.TryDequeue(out var obj))
+        {
+            obj.gameObject.SetActive(true);
+        }
+        else
+        {
+            obj = UnityEngine.Object.Instantiate(_prefab, _parent);
+        }
+
+        obj.transform.SetPositionAndRotation(position, rotation);
+
+        return obj;
+    }
+
+    internal void Recycle(T go)
+    {
+        go.gameObject.SetActive(false);
+
+        _pool.Enqueue(go);
+    }
+
+    private void FillPool(int initialPoolSize)
+    {
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            var newGO = UnityEngine.Object.Instantiate(_prefab, _parent);
+
+            newGO.gameObject.SetActive(false);
+
+            _pool.Enqueue(newGO);
+        }
+    }
+}
---
+++ Assets/Code/Services/Factories/ResourceFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal class ResourceFactory : MonoSingleton<ResourceFactory>
+{
+    [SerializeField] private Resource _resourcePrefab;
+    [SerializeField, Min(1)] private int _poolSize = 1;
+
+    private Pool<Resource> _pool;
+    private List<Resource> _droppedResources;
+
+    public IReadOnlyList<Resource> DroppedResources => _droppedResources;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _pool = new Pool<Resource>(_resourcePrefab, transform, _poolSize);
+        _droppedResources = new List<Resource>();
+    }
+
+    internal Resource Get(Vector3 position, Quaternion rotation)
+    {
+        var res = _pool.Get(position, rotation);
+        res.Construct(this);
+
+        _droppedResources.Add(res);
+
+        return res;
+    }
+
+    internal void Recycle(Resource resource)
+    {
+        _droppedResources.Remove(resource);
+
+        _pool.Recycle(resource);
+    }
+}

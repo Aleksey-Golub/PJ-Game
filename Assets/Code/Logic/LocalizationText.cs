42ae011f36eebb7d08105568bb04dc629b305266
Aleksey-Golub <alekseigolub.ag@gmail.com>
2024-11-26 00:04:39 +0200

    add LocalizationText + localize tutorial objects

---
+++ Assets/Code/Logic/LocalizationText.cs
@@ -0,0 +1,24 @@
+using Code.Services;
+using UnityEngine;
+
+public class LocalizationText : MonoBehaviour
+{
+    [SerializeField] private string _key;
+    [SerializeField] private TMPro.TMP_Text _text;
+
+    private void Start()
+    {
+        LService.LanguageChanged += SetText;
+        SetText();
+    }
+
+    private void OnDestroy()
+    {
+        LService.LanguageChanged -= SetText;
+    }
+
+    private void SetText()
+    {
+        _text.text = LService.Localize(_key);
+    }
+}

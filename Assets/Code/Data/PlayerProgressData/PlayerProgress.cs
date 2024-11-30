cb408ac8b09d98bb736d632724d0863aa6df951f
Aleksey-Golub <alekseigolub.ag@gmail.com>
2024-11-30 21:37:10 +0200

    fix Bug: not saved Player speedUp

--- Assets/Code/Data/PlayerProgressData/PlayerProgress.cs
+++ Assets/Code/Data/PlayerProgressData/PlayerProgress.cs
@@ -6,12 +6,15 @@
         public UpgradeItemsProgress UpgradeItemsProgress;
         public PositionOnLevel PositionOnLevel;
         public InventoryData InventoryData;
+        public TimerData SpeedUpTimerData;
+        public float SpeedUpSpeed;
 
         public PlayerProgress(string initialLevel)
         {
             UpgradeItemsProgress = new UpgradeItemsProgress();
             PositionOnLevel = new PositionOnLevel(initialLevel);
             InventoryData = new InventoryData();
+            SpeedUpTimerData = new TimerData(false, 0, 0);
         }
     }
 }

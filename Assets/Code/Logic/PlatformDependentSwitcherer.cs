7e56138298f28062ce80d45563130d1da8b8d2c3
Aleksey-Golub <alekseigolub.ag@gmail.com>
2024-12-04 14:19:19 +0200

    fix Bug: input hint doesn't correspond Mobile platform

---
+++ Assets/Code/Logic/PlatformDependentSwitcherer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlatformDependentSwitcherer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _desktopOn;
+    [SerializeField] private GameObject[] _mobileOn;
+
+    private void Start()
+    {
+        bool isMobile = Application.isMobilePlatform;
+
+        foreach (GameObject go in _mobileOn)
+            go.SetActive(isMobile);
+
+        foreach (GameObject go in _desktopOn)
+            go.SetActive(!isMobile);
+    }
+}

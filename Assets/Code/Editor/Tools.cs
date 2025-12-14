dbecae6b4a3bdd7d79e1eb4973fa9917e636e858
Aleksey-Golub <alekseigolub.ag@gmail.com>
2025-12-14 19:39:27 +0200

    add: 'set PROGRESS prefs' feature

--- Assets/Code/Editor/Tools.cs
+++ Assets/Code/Editor/Tools.cs
@@ -33,5 +33,23 @@ namespace Code.Editor
             PlayerPrefs.DeleteKey(SaveLoadAnalyticService.ANALYTIC);
             PlayerPrefs.Save();
         }
+        
+        [MenuItem("Tools/Set PROGRESS Prefs/Load 1.5.0 full completed game (before end of 1st location)")]
+        public static void Load_1_5_0_Save_Before_FirstPrize()
+        {
+            string savePath = "Assets/Debug Saves/1.5.0 PJ VK save (before First Prize).json";
+            string progressJSON = AssetDatabase.LoadAssetAtPath<TextAsset>(savePath).text; ;
+            PlayerPrefs.SetString(SaveLoadService.PROGRESS_KEY, progressJSON);
+            PlayerPrefs.Save();
+        }
+        
+        [MenuItem("Tools/Set PROGRESS Prefs/Load 1.5.0 full completed game (Near First Prize)")]
+        public static void Load_1_5_0_Save_Near_FirstPrize()
+        {
+            string savePath = "Assets/Debug Saves/1.5.0 PJ VK save (near First Prize).json";
+            string progressJSON = AssetDatabase.LoadAssetAtPath<TextAsset>(savePath).text; ;
+            PlayerPrefs.SetString(SaveLoadService.PROGRESS_KEY, progressJSON);
+            PlayerPrefs.Save();
+        }
     }
 }

5cdb80855ab8a1dddaeedfb496b0b45412875974
Aleksey-Golub <alekseigolub.ag@gmail.com>
2026-01-26 19:33:28 +0200

    add "Ads Box 8 Poppy"

--- Assets/Code/Editor/Generated/G_GameObjectsIds_SimpleObject.cs
+++ Assets/Code/Editor/Generated/G_GameObjectsIds_SimpleObject.cs
@@ -27,6 +27,7 @@ public static class G_GameObjectsIds_SimpleObject
         "Ads Box 30 Grass",
         "Ads Box 5 Coal",
         "Ads Box 8 Iron Ore",
-        "Ads Box 4 Iron"
+        "Ads Box 4 Iron",
+        "Ads Box 8 Poppy"
     };
 }

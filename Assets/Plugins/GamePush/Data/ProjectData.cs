ea9b5aa4ca32bba5393823a5f25f6c3916ad83b5
Aleksey-Golub <alekseigolub.ag@gmail.com>
2026-03-29 17:57:15 +0200

    add WaitGPReady setting

--- Assets/Plugins/GamePush/Data/ProjectData.cs
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -7,7 +7,7 @@ namespace GamePush.Data
         public static string TOKEN = "3qjETudvO8XXKwa5tOfsO1DgyQ5l0GhT";
         public static bool GAMEREADY_AUTOCALL = false;
         public static bool SHOW_STICKY_ON_START = false;
-        public static bool WAIT_PLAGIN_READY = false;
+        public static bool WAIT_PLAGIN_READY = true;
         public static bool AUTO_PAUSE_ON_ADS = false;
     }
 }
